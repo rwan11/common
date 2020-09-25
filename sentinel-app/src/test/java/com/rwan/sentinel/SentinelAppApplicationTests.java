b5a0934747c9a068b50fe8c40b1131c87b9d445c
apple <1925063804@qq.com>
2020-09-25 21:24:13 +0800

    sentinel

---
+++ sentinel-app/src/test/java/com/rwan/sentinel/SentinelAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwan.sentinel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentinelAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
