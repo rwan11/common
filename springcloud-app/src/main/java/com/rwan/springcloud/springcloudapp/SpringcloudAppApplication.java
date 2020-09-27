d2005741da44618e47d224b0587af16eb2c555df
apple <1925063804@qq.com>
2020-09-27 16:30:01 +0800

    sentinel-springcloud

---
+++ springcloud-app/src/main/java/com/rwan/springcloud/springcloudapp/SpringcloudAppApplication.java
@@ -0,0 +1,13 @@
+package com.rwan.springcloud.springcloudapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudAppApplication.class, args);
+    }
+
+}
---
+++ springcloud-app/src/test/java/com/rwan/springcloud/springcloudapp/SpringcloudAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rwan.springcloud.springcloudapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
